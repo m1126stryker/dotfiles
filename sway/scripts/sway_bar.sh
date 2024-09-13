be997e5a7defc1931c6c358c0afc50d93beccea7
m1126stryker <m1126stryker@tuta.io>
2024-09-13 20:58:47 +0300

    much better uwu (changed the way handling the info file away from sed to bash's builtin arrays)

--- sway/scripts/sway_bar.sh
+++ sway/scripts/sway_bar.sh
@@ -3,17 +3,17 @@
 # Retreiving data from the output of 2 different daemon scripts running in the background to-
 # minimize the time delay resultinf from handling them all on fly
 
-FILE="$HOME/.config/sway/scripts/status_data/lazy_info"
-KBLAYOUT=$(sed '1q;d' $FILE)
-BAT=$(sed '2q;d' $FILE)
-VOL=$(sed '3q;d' $FILE )
+readarray -t STATUS < <(cat $HOME/.config/sway/scripts/status_data/lazy_info)
+KBLAYOUT=${STATUS[0]}
+BAT=${STATUS[1]}
+VOL=${STATUS[2]}
 DATETIME="  $(date +'%a %Y-%m-%d %R:%S')"
-PLAYERCTL=$(sed	'4q;d' $FILE)
-NETWORKSTATUS=$(sed '5q;d' $FILE)
-BLUETOOTHSTATUS=$(sed '6q;d' $FILE)
-CPUTEMP=$(sed '7q;d' $FILE)
-WEATHER=$(sed '8q;d' $FILE)
-SPACES=$(sed '9q;d' $FILE)
+PLAYERCTL=${STATUS[3]}
+NETWORKSTATUS=${STATUS[4]}
+BLUETOOTHSTATUS=${STATUS[5]}
+CPUTEMP=${STATUS[6]}
+WEATHER=${STATUS[7]}
+SPACES=${STATUS[8]}
 
 echo "$PLAYERCTL\
 $SPACES\
