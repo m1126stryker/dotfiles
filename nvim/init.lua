d223c5e66bed8f05ec3d62ec03f43fb9565668b6
m1126stryker <m1126stryker@tuta.io>
2024-09-18 17:21:04 +0300

    basic nvim config added - init.lua -

---
+++ nvim/init.lua
@@ -0,0 +1,41 @@
+-- almost the same as kickstart.nvim since im still exploring :))
+
+-- BASIC LAYOUT -- 
+
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+vim.g.have_nerd_font = true
+
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+vim.opt.mouse = 'a'
+vim.opt.showmode = false
+vim.opt.breakindent = true
+vim.opt.undofile = true
+
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.smartindent = true
+vim.opt.signcolumn = 'yes'
+
+vim.opt.updatetime = 250
+vim.opt.timeoutlen = 300
+
+vim.opt.splitbelow = true
+vim.opt.splitright = true
+
+vim.opt.list = true
+vim.opt.listchars = {tab = '» ',trail = '.', nbsp = '␣'}
+
+vim.opt.inccommand = 'split'
+vim.opt.cursorline = true
+vim.opt.scrolloff = 22
+
+vim.schedule(function()
+	vim.opt.clipboard = 'unnamedplus'
+end)
+
+-- KEYMAPS --
+
+
