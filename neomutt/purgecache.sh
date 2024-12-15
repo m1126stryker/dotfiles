9f82e89169e14fa8ac83c6e3c1c2d35b5130f9a5
m1126stryker <m1126stryker@tuta.io>
2024-12-15 21:11:07 +0200

    basic neomutt/mutt setup added

---
+++ neomutt/purgecache.sh
@@ -0,0 +1,13 @@
+## In KB.
+CACHE_LIMIT=51200
+
+cd "$1"
+[ $? -ne 0 ] && exit
+
+[ $(du -s . | cut -f1 -d'	') -lt $CACHE_LIMIT ] && exit
+while IFS= read -r i; do
+	rm "$i"
+	[ $(du -s . | cut -f1 -d'	') -lt $CACHE_LIMIT ] && exit
+done <<EOF
+$(find . -type f -exec ls -rt1 {} +)
+EOF
